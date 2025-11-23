313cd1af806c1c0cee9b5b57196e1c871d622463
TristianPaquette <155991405+TristianPaquette@users.noreply.github.com>
2025-11-22 21:32:36 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,14 @@
-import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  //   // COPY this from your Firebase Console
-  //   apiKey: "your-api-key-goes-here",
-  //   authDomain: "your-project-name-here.firebaseapp.com",
-  //   databaseURL: "https://your-project-name-here.firebaseio.com",
-  //   projectId: "your-project-name-here",
-  //   storageBucket: "your-project-name.appspot.com",
-  //   messagingSenderId: "xxxxxxxx",
+  apiKey: "AIzaSyAwRx-X0Y6AgookvlrUmJbIl2JUFr2Y5z8",
+  authDomain: "cis371-assignment4-89bab.firebaseapp.com",
+  projectId: "cis371-assignment4-89bab",
+  storageBucket: "cis371-assignment4-89bab.appspot.com",
+  messagingSenderId: "795359785134",
+  appId: "1:795359785134:web:07543e373152f280a2fd3f",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default db;
+export const db = getFirestore(app);
