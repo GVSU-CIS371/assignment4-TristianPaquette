a481feff69782c98406def38f3771564cbd967ac
TristianPaquette <155991405+TristianPaquette@users.noreply.github.com>
2025-12-01 19:45:33 -0500

    Update Beverage.vue

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -4,33 +4,30 @@
     <Hot v-else />
 
     <Contents>
-      <!-- Top: Creamer first, otherwise Syrup -->
       <template v-slot:top>
-        <Creamer v-if="creamer !== 'No Cream'" :type="creamer" />
-        <Syrup v-else-if="syrup !== 'No Syrup'" :type="syrup" />
+        <Creamer v-if="creamer !== 'No Cream'" :type="creamer || ''" />
+        <Syrup v-else-if="syrup !== 'No Syrup'" :type="syrup || ''" />
       </template>
 
-      <!-- Middle: Syrup only if both creamer + syrup chosen -->
       <template v-slot:mid>
         <Syrup
           v-if="creamer !== 'No Cream' && syrup !== 'No Syrup'"
-          :type="syrup"
+          :type="syrup || ''"
         />
       </template>
 
-      <!-- Bottom: base -->
       <template v-slot:bottom>
-        <Base :type="base" />
+        <Base :type="base || ''" />
       </template>
     </Contents>
   </Mug>
 </template>
 
 <script setup lang="ts">
-import Contents from "./Contents.vue";
 import Mug from "./Mug.vue";
 import Cold from "./Cold.vue";
 import Hot from "./Hot.vue";
+import Contents from "./Contents.vue";
 import Creamer from "./Creamer.vue";
 import Syrup from "./Syrup.vue";
 import Base from "./Base.vue";
