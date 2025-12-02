5a626273293ec2242325b796e40ec0ca71e43f98
TristianPaquette <155991405+TristianPaquette@users.noreply.github.com>
2025-12-01 19:47:07 -0500

    Update beverageStore.ts

--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -1,41 +1,83 @@
 import { defineStore } from "pinia";
-import {
-  BaseBeverageType,
-  CreamerType,
-  SyrupType,
-  BeverageType,
-} from "../types/beverage";
-import tempretures from "../data/tempretures.json";
-import db from "../firebase.ts";
-import {
-  collection,
-  getDocs,
-  setDoc,
-  doc,
-  QuerySnapshot,
-  QueryDocumentSnapshot,
-  onSnapshot,
-} from "firebase/firestore";
+import { collection, getDocs, addDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+
+interface Ingredient {
+  id: string;
+  name: string;
+  color: string;
+}
+
+interface Beverage {
+  id?: string;
+  name: string;
+  temp: string;
+  base: Ingredient;
+  creamer: Ingredient;
+  syrup: Ingredient;
+}
 
 export const useBeverageStore = defineStore("BeverageStore", {
   state: () => ({
-    temps: tempretures,
-    currentTemp: tempretures[0],
-    bases: [] as BaseBeverageType[],
-    currentBase: null as BaseBeverageType | null,
-    syrups: [] as SyrupType[],
-    currentSyrup: null as SyrupType | null,
-    creamers: [] as CreamerType[],
-    currentCreamer: null as CreamerType | null,
-    beverages: [] as BeverageType[],
-    currentBeverage: null as BeverageType | null,
-    currentName: "",
+    temps: ["Hot", "Cold"],
+
+    bases: [] as Ingredient[],
+    creamers: [] as Ingredient[],
+    syrups: [] as Ingredient[],
+
+    currentTemp: "Hot",
+    currentBase: null as Ingredient | null,
+    currentCreamer: null as Ingredient | null,
+    currentSyrup: null as Ingredient | null,
+
+    beverageName: "",
+    savedBeverages: [] as Beverage[],
   }),
 
   actions: {
-    init() {},
-    makeBeverage() {},
+    async init() {
+      const b = await getDocs(collection(db, "bases"));
+      this.bases = b.docs.map((x) => x.data() as Ingredient);
 
-    showBeverage() {},
+      const c = await getDocs(collection(db, "creamers"));
+      this.creamers = c.docs.map((x) => x.data() as Ingredient);
+
+      const s = await getDocs(collection(db, "syrups"));
+      this.syrups = s.docs.map((x) => x.data() as Ingredient);
+
+      this.currentBase = this.bases[0];
+      this.currentCreamer = this.creamers[0];
+      this.currentSyrup = this.syrups[0];
+
+      onSnapshot(collection(db, "beverages"), (snap) => {
+        this.savedBeverages = snap.docs.map((d) => ({
+          id: d.id,
+          ...(d.data() as Beverage),
+        }));
+      });
+    },
+
+    async makeBeverage() {
+      if (!this.beverageName.trim()) return;
+
+      const newBev = {
+        name: this.beverageName,
+        temp: this.currentTemp,
+        base: this.currentBase!,
+        creamer: this.currentCreamer!,
+        syrup: this.currentSyrup!,
+      };
+
+      await addDoc(collection(db, "beverages"), newBev);
+
+      this.beverageName = "";
+    },
+
+    showBeverage(b: Beverage) {
+      this.currentTemp = b.temp;
+      this.currentBase = b.base;
+      this.currentCreamer = b.creamer;
+      this.currentSyrup = b.syrup;
+    },
   },
 });
